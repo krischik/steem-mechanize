88a490b5f1dd280cc933155ace0f6547c213bb7a
Anthony Martin <github@martin-studio.com>
2018-07-31 10:19:16 -0700

    do the thing #5 (breaks until gem update to steem-ruby-0.9.3)

--- lib/steem-mechanize.rb
+++ lib/steem-mechanize.rb
@@ -5,11 +5,5 @@ require 'mechanize'
 require 'steem/mechanize/version'
 require 'steem/mechanize/rpc/mechanize_client'
 
-# Here, we will monkeypatch steem-ruby to use Mechanize.
-module Steem
-  class Api
-    def self.default_rpc_client_class
-      Steem::Mechanize::RPC::MechanizeClient
-    end
-  end
-end
+# Here, we will inject Mechanize into steem-ruby.
+Steem::Api.register default_rpc_client_class: Steem::Mechanize::RPC::MechanizeClient
